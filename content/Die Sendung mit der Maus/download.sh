94a8d19b492e3ce905b94ddf4670baa340d219e7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-05-08 19:48:02 +0200

    download Maus

--- content/Die Sendung mit der Maus/download.sh
+++ content/Die Sendung mit der Maus/download.sh
@@ -1,1 +1,1 @@
-youtube-dl --continue --write-thumbnail --write-description https://m.youtube.com/playlist?list=PLO8lnEN5VWhO0Raa5q118BHwPle_E9sjt
+youtube-dl --continue --ignore-errors --write-thumbnail --write-description https://youtu.be/opXI6Yg0GI8 https://m.youtube.com/playlist?list=PLmZR_5AswGw9OgTRNSn3-y2dlaoRgQq-h
