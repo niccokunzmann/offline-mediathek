cff8a0de8ed67338339ed559a5f845879f7e52fa
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-05-08 19:47:13 +0200

    sh gereation broken

--- generate-index.sh
+++ generate-index.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo 'ALL_FILES = [' > index.js
+(
+  echo 'FILE_NAMES = []; F=function(s){FILE_NAMES.push(s)};'
+  echo 'DESCRIPTIONS = {};'
+) > index.js
 find content | while read file; do
   echo -n .
-  echo -n '  "' >> index.js
-  echo -n "$file" | sed 's/"/\\"/' >> index.js
-  echo '",' >> index.js
+  if echo "$file" | grep -qE '\.description$'; then
+    (
+      echo -n "DESCRIPTIONS['`echo \"$file\" | sed 's/'/\\''`'] = '"
+      cat "$file" | sed 's/\n/\\n/' | sed "s/'/\\'/"
+      echo "';"
+    ) >> index.js
+    break
+  else
+    (
+      echo -n 'F("'
+      echo "$file" | sed 's/"/\\"/'
+      echo '");'
+    ) >> index.js
+  fi
 done
 
 echo '];' >> index.js
