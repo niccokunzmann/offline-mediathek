1a57b1a6512316b2b95f91916242bae17b955ac7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-05-08 19:55:27 +0200

    add index.js from python sript

--- generate-index.py
+++ generate-index.py
@@ -12,6 +12,7 @@ add = {}
 def add_picture(entry, path):
     entry["thumb"] = path
 add[".jpg"] = add_picture
+
 def add_video(entry, path):
     entry["video"] = path
 add[".mp4"] = add_video
@@ -19,6 +20,14 @@ add[".mp4"] = add_video
 def add_description(entry, path):
     with open(path) as file:
         entry["description"] = file.read()
+add[".description"] = add_description
+
+def add_nothing(entry, path):
+    pass
+add[".sh"] = add_nothing
+
+def add_default(entry, path):
+    print("skip", path)
 
 index = {}
 for path, dirnames, fnames in os.walk(CONTENT):
@@ -27,8 +36,10 @@ for path, dirnames, fnames in os.walk(CONTENT):
         index.setdefault(fid, {})
         entry = index[fid]
         entry_path = os.path.join(path, fname)
-        add[ext](entry, entry_path)
+        func = add.get(ext, add_default)
+        func(entry, entry_path)
         print(".", end="")
+print()
 
 with open(INDEX, "w") as file:
-    json.dump(index, file)
+    json.dump(index, file, indent="  ")
